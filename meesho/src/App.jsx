f12187c6a7302d4399b3d8b8c84fa103fd550643
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-07-07 23:58:03 +0530

    github

--- meesho/src/App.jsx
+++ meesho/src/App.jsx
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './Global/Navbar';
+import Homepage from './Component/Homepage';
+import Register from './Component/Register';
 
 function App() {
   return (
     <>
-    {<Navbar/>}
-    <Routes>
-      {/* <Route exact path='' element={</>}></Route> */}
-    </Routes>
-
+      {<Navbar />}
+      <Routes>
+        {/* <Route exact path='/' element={</>}></Route > */}
+          <Route exact path='/register' element={<Register />}></Route>
+      </Routes>  
     </>
   );
 }
---
+++ meesho/src/Component/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Css/Register.css'
+
+const Register = () => {
+  return (
+    <div className='register-fullpage'>
+      
+    </div>
+  )
+}
+
+export default Register
